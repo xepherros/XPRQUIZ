925325d69c8fd3853630f21dc05ea0125eebb801
xepherros <xepherros@gmail.com>
2025-06-05 04:26:50 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css';  // โหลดก่อน
+import './style.css';  // โหลดทีหลัง
 import MainRouter from './MainRouter'; // เปลี่ยนจาก './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
