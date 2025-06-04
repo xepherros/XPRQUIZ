4977b33c047da68721e683c89131b6fb349a2a8b
xepherros <xepherros@gmail.com>
2025-06-05 00:26:37 +0700

    Update Game.jsx

--- src/Game.jsx
+++ src/Game.jsx
@@ -84,8 +84,6 @@ export default function Game({ week, nickname, goHome }) {
               className={`p-3 rounded-xl cursor-pointer shadow border text-sm bg-white
                 ${matchedPairs.includes(term.text)
                   ? 'bg-green-500 text-white font-bold'
-                  : selectedTerm?.text === term.text
-                    ? 'bg-yellow-300'
                     : ''}
               `}
 
@@ -110,8 +108,6 @@ export default function Game({ week, nickname, goHome }) {
               className={`p-3 rounded-xl cursor-pointer shadow border text-sm bg-white
                 ${matchedPairs.includes(def.pair)
                   ? 'bg-green-500 text-white font-bold'
-                  : selectedTerm?.pair === def.text
-                    ? 'bg-blue-100'
                     : ''}
               `}
 
