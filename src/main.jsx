125b1fb5d8c9d1dd698378b4fcfd14df0b884ba7
xepherros <xepherros@gmail.com>
2025-06-05 03:49:56 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import MainRouter from './MainRouter'; // เปลี่ยนจาก './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MainRouter />
   </React.StrictMode>
 );
