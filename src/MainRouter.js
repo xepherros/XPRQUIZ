489a7561a8d586365da7eafebd965a469acdad04
xepherros <xepherros@gmail.com>
2025-06-05 04:48:11 +0700

    Update MainRouter.js

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import MainMenu from "./MainMenu";
 import AppSpelling from "./AppSpelling";
-import AppVocab from "./AppVocab"; // ต้อง import ทุกคอมโพเนนต์ที่ใช้
+import AppVocab from "./AppVocab";
 
 export default function MainRouter() {
   const [screen, setScreen] = useState("menu");
   const goHome = () => setScreen("menu");
 
   return (
-    <div>
+    <>
       {screen === "menu" && <MainMenu onSelect={setScreen} />}
       {screen === "spelling" && <AppSpelling goHome={goHome} />}
       {screen === "vocab" && <AppVocab goHome={goHome} />}
-    </div>
+    </>
   );
 }
